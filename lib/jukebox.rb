fc8751cb87a07bde2889bf0b155685053c81fef6
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-07 03:04:44 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,7 +28,7 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  user_input = gets.chomp
+  user_input = gets.strip
   if user_input.to_i >= 1 && user_input.to_i <= songs.length
     puts "Playing #{songs[user_input.to_i - 1]}"
   elsif songs.include?(user_input)
